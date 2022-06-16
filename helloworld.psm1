435a7a8257ec25a223f1b3feebd27ca0203f7d9b
Thirukrishnan <63901950+Thirukrishnan@users.noreply.github.com>
2022-06-16 21:48:05 +0530

    Create helloworld.psm1

---
+++ helloworld.psm1
@@ -0,0 +1,2 @@
+#A module in powershell is used to share code or script between computers easily!The only difference I know is that the extension changes and the module must be imported (Import-Module <ModuleName>) before using the functions or variables in the script.
+write-host "Hello World"
